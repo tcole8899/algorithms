8978c3e4df1c2be0413c13be35635c562b3c377f
tcole8899 <tcole8899@gmail.com>
2019-06-20 16:48:22 -0400

    new

--- source/Linear/Stack.cpp
+++ source/Linear/Stack.cpp
@@ -1,1 +1,61 @@
 #include "Stack.hpp"
+
+Stack::Stack(){
+    head =  nullptr;
+    len = 0;
+};
+
+Stack::~Stack(){
+    clear();
+};
+
+/** size()
+* Returns the size of the stack.
+*/
+unsigned int Stack::size(){
+    return len;
+};
+
+/** push(int data)
+   * Pushes a node containing the given data to the top of the stack.
+   */
+void Stack::push(int data){
+    Node* n = new Node(data);
+    if(len == 0) head = n;
+    else{
+        Node* temp = head;
+        while(temp) temp = temp->next;
+        temp->next = n;
+        n -> next = head;
+        head = n;
+    }
+};
+
+/** pop()
+   * Removes and returns the data at the top of the stack.
+   */
+int Stack::pop(){
+    int result = head -> data;
+    if(len == 0) return;
+    else if(len == 1) return result;
+    else{
+        Node* temp = head;
+        while(temp) temp = temp -> next;
+    }
+
+};
+
+/** top()
+   * Returns the data at the top of the stack without changing the stack.
+   */
+int top();
+
+/** empty()
+   * Returns whether or not the stack is empty.
+   */
+bool empty();
+
+/** clear()
+   * Clears the entire stack.
+   */
+void clear();
